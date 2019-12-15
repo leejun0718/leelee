b149148d373a6c965b9ab0a706177fc8c064da6e
leeju <leeju@172.30.1.12>
2019-12-15 19:44:45 +0900

    ㅇㅇ

--- kakao_emo_shop/src/182.237.126.19_teamdb2_member_insert.sql
+++
@@ -1,7 +0,0 @@
-insert into member(m_no, m_niname, m_id, m_pass, m_phone) values(MEMBER_M_NO_SEQ.nextval, 'java1', 'java1a', 'java1a', '111-1111-1111');
-insert into member(m_no, m_niname, m_id, m_pass, m_phone) values(MEMBER_M_NO_SEQ.nextval, 'java2', 'java2a', 'java2a', '222-2222-2222');
-insert into member(m_no, m_niname, m_id, m_pass, m_phone) values(MEMBER_M_NO_SEQ.nextval, 'java3', 'java3a', 'java3a', '333-3333-3333');
-insert into member(m_no, m_niname, m_id, m_pass, m_phone) values(MEMBER_M_NO_SEQ.nextval, 'java4', 'java4a', 'java4a', '444-4444-4444');
-insert into member(m_no, m_niname, m_id, m_pass, m_phone) values(MEMBER_M_NO_SEQ.nextval, 'java5', 'java5a', 'java5a', '555-5555-5555');
-
-commit;
